2f3c6fa2a7d8c0e065b6cd1a56f5b61e37f00d7e
devZahid <officialzahidhasan19932021@gmail.com>
2023-10-12 15:10:53 +0600

    adding rating in the project

--- src/components/home/TabItems.jsx
+++ src/components/home/TabItems.jsx
@@ -2,27 +2,33 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import TabCard from "./TabCard";
 import { useEffect, useState } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const TabItems = () => {
-    
-    const [toys,setToys] = useState([]);
+  const [toys, setToys] = useState([]);
 
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
-    useEffect(() => {
-        fetch('alltoys.json')
-        .then(res => res.json())
-        .then(data => {
-            setToys(data)
-        })
-    }, [])
-    // console.log('this is our toys', toys);
-    const carToys = toys.filter(car => car.category === 'car');
-    const superHeros = toys.filter(superHero => superHero.category === 'superhero');
-    const softToys = toys.filter(softToy => softToy.category === 'softtoy');
-    const scineceToys = toys.filter(science => science.category === 'science');
-    
+  useEffect(() => {
+    fetch("alltoys.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setToys(data);
+      });
+  }, []);
+  // console.log('this is our toys', toys);
+  const carToys = toys.filter((car) => car.category === "car");
+  const superHeros = toys.filter(
+    (superHero) => superHero.category === "superhero"
+  );
+  const softToys = toys.filter((softToy) => softToy.category === "softtoy");
+  const scineceToys = toys.filter((science) => science.category === "science");
+
   return (
-    <div>
+    <div className="mb-20">
       <Tabs>
         <TabList>
           <Tab>Cars</Tab>
@@ -32,32 +38,32 @@ const TabItems = () => {
         </TabList>
         {/* give tab contents here */}
         <TabPanel>
-         <div className="md:grid md:grid-cols-4 gap-5 px-8">
-         {
-            carToys.map((car) => <TabCard key={car.id} item={car}></TabCard>)
-          } 
-         </div>
+          <div data-aos="fade-up" className="md:grid md:grid-cols-4 gap-5 px-8">
+            {carToys.map((car) => (
+              <TabCard key={car.id} item={car}></TabCard>
+            ))}
+          </div>
         </TabPanel>
         <TabPanel>
-        <div className="md:grid md:grid-cols-4 gap-5 px-8">
-         {
-            superHeros.map((superHero) => <TabCard key={superHero.id} item={superHero}></TabCard>)
-          } 
-         </div>
+          <div data-aos="fade-up" className="md:grid md:grid-cols-4 gap-5 px-8">
+            {superHeros.map((superHero) => (
+              <TabCard key={superHero.id} item={superHero}></TabCard>
+            ))}
+          </div>
         </TabPanel>
         <TabPanel>
-        <div className="md:grid md:grid-cols-4 gap-5 px-8">
-         {
-            softToys.map((softToy) => <TabCard key={softToy.id} item={softToy}></TabCard>)
-          } 
-         </div>
+          <div data-aos="fade-up" className="md:grid md:grid-cols-4 gap-5 px-8">
+            {softToys.map((softToy) => (
+              <TabCard key={softToy.id} item={softToy}></TabCard>
+            ))}
+          </div>
         </TabPanel>
         <TabPanel>
-        <div className="md:grid md:grid-cols-4 gap-5 px-8">
-         {
-            scineceToys.map((scienceToy) => <TabCard key={scienceToy.id} item={scienceToy}></TabCard>)
-          } 
-         </div>
+          <div data-aos="fade-up" className="md:grid md:grid-cols-4 gap-5 px-8">
+            {scineceToys.map((scienceToy) => (
+              <TabCard key={scienceToy.id} item={scienceToy}></TabCard>
+            ))}
+          </div>
         </TabPanel>
       </Tabs>
     </div>
