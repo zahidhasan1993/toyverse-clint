64d5aab8173e1a21cb56f6214919726be8cedff6
devZahid <officialzahidhasan19932021@gmail.com>
2023-10-13 22:13:24 +0600

    add toy form function added

--- src/components/allPages/AddToy.jsx
+++ src/components/allPages/AddToy.jsx
@@ -1,45 +1,150 @@
 import useAuth from "../../customHooks/useAuth";
+import useTitle from "../../customHooks/useTitle";
 
 const AddToy = () => {
-    const {user} = useAuth();
-    // console.log(user?.email);
-    const handleSubmit = () => {
-        console.log('paise');
-    }
+  const { user } = useAuth();
+  useTitle("ToyVerse | addtoy");
+  // console.log(user?.email);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const photo = form.photoUrl.value;
+    const category = form.category.value;
+    const price = form.price.value;
+    const rating = form.rating.value;
+    const details = form.details.value;
+
+    const formData = {
+      name,
+      email,
+      photo,
+      category,
+      price,
+      rating,
+      details
+    };
+    console.log(formData);
+  };
   return (
-  <div className="py-32">
-    <section className="p-6 bg-white rounded-md shadow-md dark:bg-gray-800">
-    <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">Add Toy</h2>
+    <div className="py-32">
+      <section className="p-6 bg-white rounded-md shadow-md dark:bg-gray-800">
+        <h2 className="text-2xl font-semibold text-gray-700 capitalize dark:text-white">
+          Add Toy
+        </h2>
 
-    <form className="h-full" onSubmit={handleSubmit}>
-        <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+        <form onSubmit={handleSubmit} className="h-full">
+          <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
             <div>
-                <label className="text-gray-700 dark:text-gray-200" htmlFor="username">Toyname</label>
-                <input type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="username"
+              >
+                Toyname
+              </label>
+              <input
+                name="name"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              />
             </div>
 
             <div>
-                <label className="text-gray-700 dark:text-gray-200" htmlFor="emailAddress">User Email Email</label>
-                <input type="email" defaultValue={user?.email} className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="emailAddress"
+              >
+                User Email
+              </label>
+              <input
+                name="email"
+                type="email"
+                defaultValue={user?.email}
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              />
             </div>
 
             <div>
-                <label className="text-gray-700 dark:text-gray-200" htmlFor="picture">Photo url</label>
-                <input  type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="picture"
+              >
+                Photo url
+              </label>
+              <input
+                name="photoUrl"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              />
             </div>
 
             <div>
-                <label className="text-gray-700 dark:text-gray-200" htmlFor="category">Category</label>
-                <input id="passwordConfirmation" type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="category"
+              >
+                Category
+              </label>
+              <input
+                name="category"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              />
             </div>
-        </div>
+            <div>
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="price"
+              >
+                Price
+              </label>
+              <input
+                name="price"
+                type="number"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              />
+            </div>
+            <div>
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="rating"
+              >
+                Ratings
+              </label>
+              <input
+                name="rating"
+                type="number"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              />
+            </div>
+          </div>
+          <div className="mt-5">
+            <label
+              className="text-gray-700 dark:text-gray-200"
+              htmlFor="details"
+            >
+              Details
+            </label>
+            <textarea
+              name="details"
+              className="w-full mt-5 resize-none"
+              cols="30"
+              rows="10"
+            ></textarea>
+          </div>
 
-        <div className="flex justify-end mt-6">
-            <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transhtmlForm bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Save</button>
-        </div>
-    </form>
-</section>
-  </div>
+          <div className="flex justify-end mt-6">
+            <button
+              type="submit"
+              className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transhtmlForm bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </section>
+    </div>
   );
 };
 
